ecf17a14230193048eafad142d325622543be5df
claudio3964 <cap3964@gmail.com>
2025-11-11 23:44:22 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,4 @@
-<script>
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
-      .then(reg => console.log('✅ Service Worker registrado correctamente.', reg))
-      .catch(err => console.error('❌ Error registrando Service Worker:', err));
-  });
-}
-</script>
+// service-worker.js
+self.addEventListener('install', e => console.log('Service Worker instalado'));
+self.addEventListener('activate', e => console.log('Service Worker activado y corriendo'));
+
