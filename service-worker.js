3ab58d34f852401aa2f2d349964ed7e441f6d2e8
claudio3964 <cap3964@gmail.com>
2025-11-14 12:10:24 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,48 @@
-// service-worker.js
-self.addEventListener('install', e => console.log('Service Worker instalado'));
-self.addEventListener('activate', e => console.log('Service Worker activado y corriendo'));
+const CACHE_NAME = 'cot-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-72.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
+// INSTALAR
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CCACHE_NAME).then(cache => {
+      console.log('Cache abierto');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// ACTIVAR
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH → responder desde caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 
