0d7710804147361bf33395493e14a5633759fdb2
claudio3964 <cap3964@gmail.com>
2025-11-16 17:15:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,39 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'cot-app-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-72.png',
+  './icons/icon-192.png', 
+  './icons/icon-512.png',
+  './icons/maskable-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open("cot-cache-v1").then((cache) => {
-      return cache.addAll([
-        "/app-kilometros-completa/",
-        "/app-kilometros-completa/index.html",
-        "/app-kilometros-completa/manifest.json",
-        "/app-kilometros-completa/icons/icon-192.png",
-        "/app-kilometros-completa/icons/icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
