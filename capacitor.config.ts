b4d46d362305f1ad51e8ca1ab1f8c3cab296372b
claudio3964 <cap3964@gmail.com>
2026-03-17 18:01:47 -0300

    checkpoint: antes de fix input origen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.driverlog.app',
-  appName: 'DriverLog',
+  appName: 'Gestión de Viajes',
   webDir: 'www'
 };
 
