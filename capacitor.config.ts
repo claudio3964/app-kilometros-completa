9510a0b5c757ba6dd08a4e94ea2eae3fc1ac0d50
claudio3964 <cap3964@gmail.com>
2026-03-17 18:15:22 -0300

    Revert "checkpoint: antes de fix input origen"
    This reverts commit b4d46d362305f1ad51e8ca1ab1f8c3cab296372b.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.driverlog.app',
-  appName: 'Gestión de Viajes',
+  appName: 'DriverLog',
   webDir: 'www'
 };
 
