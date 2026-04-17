7b9c3afa377d1e762a80d73786a3e67f72d4c9c7
claudio3964 <cap3964@gmail.com>
2026-04-17 13:18:29 -0300

    fix: panel admin usa totalsSnapshot, acoplado km en modal viajes

--- android/app/src/main/java/com/driverlog/app/MainActivity.java
+++ android/app/src/main/java/com/driverlog/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.driverlog.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        android.webkit.WebView webView = getBridge().getWebView();
+        webView.clearCache(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+    }
+}
